708e70fe5eeb2ed6385cbcc59de2e12ce41670d8
glen newton <glen.newton@gmail.com>
2016-06-05 13:02:46 -0500

    Updated README to show example; removed ZZ from output

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # struct2dot
+
+Convert Go structs to GraphViz dot format.
+
+<img src="https://gnewton.github.io/repos/struct2dot/meshNoStringsNumbers.svg">
--- struct2dot.go
+++ struct2dot.go
@@ -18,7 +18,6 @@ var numberTypes = []string{"uint8",
 	"float64",
 	"complex64",
 	"complex128",
-	"byte",
 	"rune",
 }
 
@@ -33,6 +32,7 @@ type Config struct {
 	ShowNumbers         bool
 	RemovePackagePrefix bool
 	IgnoreTypes         []string
+	ManualLinks         map[string][]string
 }
 
 type DotDriver struct {
@@ -47,30 +47,51 @@ func (d *DotDriver) PrintHeader() {
 	fmt.Println("digraph foo {")
 	fmt.Println("\tnode [color=Red]")
 	fmt.Println("\tedge [color=Blue]")
+	fmt.Println("\toverlap=false;")
+	fmt.Println("\tsplines=true;")
 }
 
 func (d *DotDriver) init() {
 	d.edges = make(map[string]bool)
 	d.ignoreTypes = make(map[string]bool)
-	if len(d.Config.IgnoreTypes) > 0 {
+	if d.Config == nil {
+		d.Config = &(Config{
+			ShowStrings:         false,
+			ShowNumbers:         false,
+			RemovePackagePrefix: true,
+			IgnoreTypes:         nil,
+		})
+	}
+	if d.Config.IgnoreTypes != nil && len(d.Config.IgnoreTypes) > 0 {
 		for i, _ := range d.Config.IgnoreTypes {
 			d.ignoreTypes[d.Config.IgnoreTypes[i]] = true
+			d.ignoreTypes["*"+d.Config.IgnoreTypes[i]] = true
 		}
 	}
-	/*
-		if !d.Config.ShowStrings {
-			d.ignoreTypes["string"] = true
-		}
 
-			if !d.Config.ShowNumbers {
-				for i, _ := range numberTypes {
-					d.ignoreTypes[numberTypes[i]] = true
-				}
-			}
-	*/
+	if !d.Config.ShowStrings {
+		d.ignoreTypes["string"] = true
+		d.ignoreTypes["[]string"] = true
+	}
+
+	if !d.Config.ShowNumbers {
+		for i, _ := range numberTypes {
+			d.ignoreTypes[numberTypes[i]] = true
+		}
+	}
 }
 
 func (d *DotDriver) PrintFooter() {
+	if d.Config.ManualLinks != nil && len(d.Config.ManualLinks) > 0 {
+		fmt.Println("\n\n### MANUAL LINKS")
+		fmt.Println("\tedge [color=Red]")
+		for src, dests := range d.Config.ManualLinks {
+			for _, dest := range dests {
+				fmt.Println(src, "->", dest)
+			}
+
+		}
+	}
 	fmt.Println("}")
 }
 
@@ -81,40 +102,53 @@ func (d *DotDriver) PrintType(t *reflect.Type) {
 func printType(parent *reflect.Type, t *reflect.Type, d *DotDriver) {
 
 	kind := (*t).Kind().String()
+	if parent != nil {
+		//fmt.Println("-----------  ", (*parent).String(), (*t).String(), "  =", kind)
+	}
 	if kind == "ptr" || kind == "slice" {
+		fmt.Println("   ###DEEP")
 		actualType := (*t).Elem()
 		printType(parent, &actualType, d)
 		return
 	}
 
+	fmt.Println("   ###KIND=", kind)
 	if kind != "struct" {
 		return
 	}
 
 	n := (*t).NumField()
-	for i := 0; i < n; i++ {
-		newT := (*t).Field(i).Type
+	if n == 0 {
+		fmt.Println("   ###NOT CHILDREN")
+	}
+	//fmt.Println("   ### 12")
 
+	for i := 0; i < n; i++ {
+		fmt.Println("#++++++++++++++++++++++", (*t).Field(i).Name)
+		newT := (*t).Field(i).Type
+		newKind := newT.Kind().String()
+
+		fmt.Println("### CHILD=", newT.String(), ":", newT.Name())
+		fmt.Println("###", d.ignoreTypes)
 		if _, ok := d.ignoreTypes[removePointerLeadingAsterisk(newT.String())]; ok {
-			return
+			fmt.Println("#################### RETURNING")
+			continue
 		}
 
-		newKind := newT.Kind().String()
 		if newKind == "ptr" || newKind == "slice" {
 			newT = newT.Elem()
 		}
 
-		if newT.Name() != "int8" && newT.Name() != "string" && newT.Name() != "int16" {
-			parentString := clean((*t).String(), d.Config.RemovePackagePrefix)
-			var child string
-			child = clean(newT.String(), d.Config.RemovePackagePrefix)
-			edgeName := parentString + "_" + child
-			if _, ok := d.edges[edgeName]; !ok {
-				fmt.Println("\t\"" + parentString + "\"->\"" + child + "\";")
-				d.edges[edgeName] = true
-			}
-			printType(t, &newT, d)
+		parentString := clean((*t).String(), d.Config.RemovePackagePrefix)
+		var child string
+		child = clean(newT.String(), d.Config.RemovePackagePrefix)
+		edgeName := parentString + "_" + child
+		if _, ok := d.edges[edgeName]; !ok {
+			fmt.Println("\t\"" + parentString + "\"[label=\"" + parentString + "\"];")
+			fmt.Println("\t\"" + parentString + "\"->\"" + child + "\";")
+			d.edges[edgeName] = true
 		}
+		printType(t, &newT, d)
 	}
 }
 
